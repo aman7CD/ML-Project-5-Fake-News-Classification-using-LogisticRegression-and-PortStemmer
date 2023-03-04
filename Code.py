85241fe1536319449fc845e1b6be017db46d73f4
Aman Chaudhry <30852046+aman7CD@users.noreply.github.com>
2023-03-04 18:10:57 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,20 +1,18 @@
 ## Importing the Dependecies 
-from nltk.corpus import stopwords
+import numpy as np 
+import pandas as pd
+import re
 
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-import re
-
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.metrics import accuracy_score
 
 import nltk
-
 nltk.download("stopwords")
 
 
